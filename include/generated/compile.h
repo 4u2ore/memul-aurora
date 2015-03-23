65ad556326d24faf8f2c50661347b2e89c067465
Aurore Paillusson <aurore.paillusson@gmail.com>
2015-03-23 18:05:59 +0100

    HTC sources 2.16.401.5 - memul-3.4.0-g3ea4692

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 14 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 SMP PREEMPT Thu Aug 21 10:16:07 CST 2014"
+#define LINUX_COMPILE_BY "jiangxu"
+#define LINUX_COMPILE_HOST "jiangxu"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
